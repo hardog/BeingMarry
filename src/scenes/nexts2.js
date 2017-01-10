3ecfda7df69562175ef3628c3a7a10569e77895f
hardog <peng.zhao@husor.com.cn>
2017-01-10 14:19:41 +0800

    adjuest

--- src/scenes/nexts2.js
+++ src/scenes/nexts2.js
@@ -87,7 +87,7 @@ var nexts2 = bm.scenes.nexts2 = Hilo.Class.create({
             delay: 1000,
             ease: self.outEase,
             onStart: function(){
-                self.text.hideAnimateRTOL();
+                bm.stage.addChild(self.text);
                 bm.loadNext('s3');
             },
             onComplete: function(){
